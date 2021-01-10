495484049f451a06c8dad7738f6f48fa0e16630a
Apollo2016926 <944644308@qq.com>
2021-01-11 00:14:29 +0800

    不需要mapper文件

--- src/main/java/com/gexx/mybatis/Mapper/EmployeeMapper.java
+++ src/main/java/com/gexx/mybatis/Mapper/EmployeeMapper.java
@@ -2,9 +2,11 @@ package com.gexx.mybatis.Mapper;
 
 
 import com.gexx.mybatis.entity.Employee;
+import org.apache.ibatis.annotations.Select;
 
 public interface EmployeeMapper {
 
+    @Select("select * from gexx_employee where id = #{id}")
     public Employee getEmployById(Integer id);
 
 }
